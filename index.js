e4e3f8107b86b58b4bc07a7a9e97ba74059296d7
rudipsq <166302374+valodex@users.noreply.github.com>
2024-04-11 13:53:58 +0200

    image lazy loading

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
 let themeArray = null;
 let skinArray = null;
 
+// setup
+document.addEventListener("DOMContentLoaded", async function () {
+    await startPage()
+});
+
+async function startPage(){
+    let jsonString_viewMode = localStorage.getItem('valodex_viewMode');
+    if(jsonString_viewMode == null) localStorage.setItem('valodex_viewMode', '{"mode": "list"}')
+    if(jsonString_viewMode == '{"mode": "grid"}'){
+        let image_viewMode = document.getElementById("viewmode_icon");
+        changeViewMode(image_viewMode);
+    }
+
+
+    let jsonString = localStorage.getItem('valodex_collected');
+    if(jsonString == null) localStorage.setItem('valodex_collected', '{"vdex_version": "1"}')
+
+    try {
+        await getApiThemes();
+        try {
+            await getApiSkins();
+            setTimeout(function() {
+                addThemesToWebsite();
+
+                const counter = document.getElementById("counter");
+                counter.textContent = updateCollectedCounter() + "/" + (skinArray.length - 2*18);
+                imageZoom();
+                sortThemes();
+              }, 1000);                  //your internet might me to slow and this website isn't optimized lol
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+
+// API calls
 async function getApiSkins(){
     fetch('https://valorant-api.com/v1/weapons/skins')
     .then(response => response.json())
     .then(responseData => {
         const dataArray = responseData.data;
         const filteredData = dataArray.map(item => {
-        // Filter out specific properties
-        if(item.hasOwnProperty("chromas")){
-            const jsonArraychromas = item.chromas;
+            // Filter out specific properties
+            if(item.hasOwnProperty("chromas")){
+                const jsonArraychromas = item.chromas;
 
-            for (let image of jsonArraychromas) {
+                for (let image of jsonArraychromas) {
+                    return {
+                        uuid: item.uuid,
+                        theme: item.themeUuid,
+                        name: item.displayName,
+                        image: image.fullRender
+                    };
+                }
+            } else{
                 return {
                     uuid: item.uuid,
                     theme: item.themeUuid,
                     name: item.displayName,
-                    image: image.fullRender
+                    image: item.displayIcon
                 };
             }
-        } else{
-            return {
-                uuid: item.uuid,
-                theme: item.themeUuid,
-                name: item.displayName,
-                image: item.displayIcon
-            };
-        }
-    });
-    
-    const jsonString = JSON.stringify(filteredData);
+        });
+        
+        const jsonString = JSON.stringify(filteredData);
 
-    skinArray = JSON.parse(jsonString);
+        skinArray = JSON.parse(jsonString);
+
+        console.log("DEBUG: skinArray:", skinArray);
     })
     .catch(error => {
         console.log('Error:', error);
     });
 }
-
 
 async function getApiThemes(){
     fetch('https://valorant-api.com/v1/themes')
@@ -45,17 +86,18 @@ async function getApiThemes(){
     .then(responseData => {
         const dataArray = responseData.data;
         const filteredData = dataArray.map(item => {
-        // Filter out specific subcategories or properties
-        return {
-            theme: item.uuid,
-            name: item.displayName
-        };
-    });
+            // Filter out specific subcategories or properties
+            return {
+                theme: item.uuid,
+                name: item.displayName
+            };
+        });
     
-    const jsonString = JSON.stringify(filteredData);
-    //console.log(jsonString); // Display the JSON string in the console
+        const jsonString = JSON.stringify(filteredData);
+        //console.log(jsonString); // Display the JSON string in the console
 
-    themeArray = JSON.parse(jsonString);
+        themeArray = JSON.parse(jsonString);
+        console.log("DEBUG: themeArray:", themeArray);
     })
     .catch(error => {
         console.log('Error:', error);
@@ -63,132 +105,184 @@ async function getApiThemes(){
 }
 
 
+
 function addThemesToWebsite(){
     const container = document.getElementById("themes_container");
+
+    let themeNames = new Array(); // testing: combining themes with the same name
 
     // Iterate over the JSON array
     for (let item of themeArray) {
         // Create a new div element
-        const div = document.createElement("div");
-        div.classList.add("themes")
+        // console.log(item.name)
 
-        const heading = document.createElement("h3");
+        let div;
+        let heading;
+
+        if(themeNames.includes(item.name) && false){ // testing: combining themes with the same name
+            div = document.getElementById(item.name)
+            heading = div.querySelector('h3')
+            heading = document.createElement("h3");
+        } else {
+            div = document.createElement("div");
+            div.id = item.name
+            div.classList.add("themes");
+            heading = document.createElement("h3");
+        }
+
+        themeNames.push(item.name);
 
         const table = document.createElement("table");
 
         // for all skins with theme_uuid = uuid of current theme
         let currentUuidSkins = searchArrayByKey(skinArray, "theme", item.theme)
+
+
         for (let skin of currentUuidSkins){
 
-            //heading
-            if((skin.name).includes(item.name)){
-                heading.textContent = item.name;
-            } else if(skin.name == "Prism III Axe"){
-                heading.textContent = "Prism2";
-            } else if(!skin.name.includes(weaponNameFromString(skin.name))){
-                //heading.textContent = skin.name + " (" + item.name + ")";
-            } else{
-                var regex = new RegExp(weaponNameFromString(skin.name), "gi");
-                heading.textContent = (skin.name).replace(regex, "") + " (" + item.name + ")";
-                //heading.textContent = skin.name + " (" + item.name + ")";
-            }
+            let content = addWeaponsToTheme(skin, heading, item, table);
 
-            const tr = document.createElement("tr");
-
-            //name
-            const td_name = document.createElement("td");
-            td_name.classList.add("name_td");
-
-            var weaponName = skin.name;
-            if(weaponNameFromString(skin.name) != null){
-                weaponName = weaponNameFromString(skin.name);
-            }
-            td_name.textContent = weaponName;
-        
-            //image
-            const td_image = document.createElement("td");
-            td_image.classList.add("image_td");
-            const image = document.createElement("img");
-            image.classList.add("image_skin");
-            image.loading = "lazy";
-            
-            if(weaponName == "Phantom"){
-                image.style.height = "38px";
-            }
-            if(weaponName == "Ghost" || weaponName == "Guardian" || weaponName == "Marshal"){
-                image.style.height = "36px";
-            }
-            if(weaponName == "Shorty"){
-                image.style.height = "32px";
-            }
-            if(weaponName == "Bucky"){
-                image.style.height = "28px";
-            }
-
-            const img = new Image();
-            img.src = skin.image;
-            img.onload = function() {
-                image.src = skin.image;
-            };
-            img.onerror = function() {
-                image.src = "images/icons/skins_noGun.png";
-            };
-            td_image.appendChild(image);
-
-            //button
-            const td_button = document.createElement("td");
-            td_button.classList.add("collect_button_td");
-            const button = document.createElement("button");
-
-            let jsonString = localStorage.getItem('valodex_collected');
-            if(jsonString.includes(skin.uuid+'":true')){
-                button.textContent = 'remove';
-                button.classList.add("remove_collection_button");
-            } else{
-                button.textContent = 'add';
-                button.classList.add("add_collection_button");
-            }
-            button.addEventListener('click', function() {
-                triggerCollected(skin.uuid);
-              });
-            td_button.appendChild(button);
-
-            tr.appendChild(td_name)
-            tr.appendChild(td_image)
-            tr.appendChild(td_button)
-            table.appendChild(tr)
+            div.appendChild(content[0]);
+            div.appendChild(content[1])
+            if(currentUuidSkins.length > 0 && item.name != "Random" && item.name != "Standard") container.appendChild(div);
         }
-
-        div.appendChild(heading);
-        div.appendChild(table)
-        if(currentUuidSkins.length > 0 && item.name != "Random" && item.name != "Standard") container.appendChild(div);
+            
     }
+
+    // Function to lazy load images
+    function lazyLoadImages(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting || entry.intersectionRatio > 0) {
+                const image = entry.target;
+                image.src = image.dataset.src; // Load the image
+                observer.unobserve(image); // Stop observing the image
+            }
+        });
+    }
+    const observerConfig = {
+        rootMargin: '200px',
+    };
+
+    const observer = new IntersectionObserver(lazyLoadImages, observerConfig);
+    const images = document.querySelectorAll('.image_skin');
+    images.forEach(image => {
+        observer.observe(image);
+    });
 }
+
+function addWeaponsToTheme(skin, heading, item, table){
+    //heading
+    heading.textContent = item.name;
+    if(!skin.name.includes(weaponNameFromString(skin.name))){
+    } else if(!(skin.name).includes(item.name)) {
+        var regex = new RegExp(weaponNameFromString(skin.name), "gi");
+        heading.textContent = (skin.name).replace(regex, "") + " (" + item.name + ")";
+    }
+
+    const tr = document.createElement("tr");
+
+    //name
+    const td_name = document.createElement("td");
+    td_name.classList.add("name_td");
+
+    var weaponName = skin.name;
+    if(weaponNameFromString(skin.name) != null){
+        weaponName = weaponNameFromString(skin.name);
+    }
+    td_name.textContent = weaponName;
+
+
+    // image
+    const td_image = document.createElement("td");
+    td_image.classList.add("image_td");
+    const image = document.createElement("img");
+    image.classList.add("image_skin");
+    image.src = "images/icons/skins_noGun.png"; // Placeholder image, remove to show no image
+    image.dataset.src = skin.image; // Store actual image URL in a data attribute
+
+    // Set image height based on weaponName
+    if (weaponName == "Phantom") {
+        image.style.height = "38px";
+    } else if (weaponName == "Ghost" || weaponName == "Guardian" || weaponName == "Marshal") {
+        image.style.height = "36px";
+    } else if (weaponName == "Shorty") {
+        image.style.height = "32px";
+    } else if (weaponName == "Bucky") {
+        image.style.height = "28px";
+    }
+
+    image.onerror = function() {
+        // If there's an error loading the image, display the default placeholder image
+        image.src = "images/icons/skins_noGun.png";
+        image.style.height = "38px";
+    };
+
+    // Append the image to the table cell
+    td_image.appendChild(image);
+
+
+    //button
+    const td_button = document.createElement("td");
+    td_button.classList.add("collect_button_td");
+    const button = document.createElement("button");
+
+    let jsonString = localStorage.getItem('valodex_collected');
+    if(jsonString.includes(skin.uuid+'":true')){
+        button.textContent = 'remove';
+        button.classList.add("remove_collection_button");
+    } else{
+        button.textContent = 'add';
+        button.classList.add("add_collection_button");
+    }
+    button.addEventListener('click', function() {
+        triggerCollected(skin.uuid);
+      });
+    td_button.appendChild(button);
+
+    tr.appendChild(td_name)
+    tr.appendChild(td_image)
+    tr.appendChild(td_button)
+    table.appendChild(tr)
+
+    return [heading, table];
+}
+
 
 function searchArrayByKey(jsonArray, key, value) {
     return jsonArray.filter(obj => obj[key] === value);
 }
 
-function weaponNameFromString(string){
-    if (string.includes("Classic")) return "Classic"
-    else if (string.includes("Shorty")) return "Shorty"
-    else if (string.includes("Frenzy")) return "Frenzy"
-    else if (string.includes("Ghost")) return "Ghost"
-    else if (string.includes("Sheriff")) return "Sheriff"
-    else if (string.includes("Stinger")) return "Stinger"
-    else if (string.includes("Spectre")) return "Spectre"
-    else if (string.includes("Bucky")) return "Bucky"
-    else if (string.includes("Judge")) return "Judge"
-    else if (string.includes("Bulldog")) return "Bulldog"
-    else if (string.includes("Guardian")) return "Guardian"
-    else if (string.includes("Phantom")) return "Phantom"
-    else if (string.includes("Vandal")) return "Vandal"
-    else if (string.includes("Marshal")) return "Marshal"
-    else if (string.includes("Operator")) return "Operator"
-    else if (string.includes("Ares")) return "Ares"
-    else if (string.includes("Odin")) return "Odin"
-    else if (string.includes("Winterwunderland Candy Cane")) return "Winter-wunderland Candy Cane"
-    //else return string
+// function weaponNameFromString(string){
+//     if (string.includes("Classic")) return "Classic"
+//     else if (string.includes("Shorty")) return "Shorty"
+//     else if (string.includes("Frenzy")) return "Frenzy"
+//     else if (string.includes("Ghost")) return "Ghost"
+//     else if (string.includes("Sheriff")) return "Sheriff"
+//     else if (string.includes("Stinger")) return "Stinger"
+//     else if (string.includes("Spectre")) return "Spectre"
+//     else if (string.includes("Bucky")) return "Bucky"
+//     else if (string.includes("Judge")) return "Judge"
+//     else if (string.includes("Bulldog")) return "Bulldog"
+//     else if (string.includes("Guardian")) return "Guardian"
+//     else if (string.includes("Phantom")) return "Phantom"
+//     else if (string.includes("Vandal")) return "Vandal"
+//     else if (string.includes("Marshal")) return "Marshal"
+//     else if (string.includes("Outlaw")) return "Outlaw"
+//     else if (string.includes("Operator")) return "Operator"
+//     else if (string.includes("Ares")) return "Ares"
+//     else if (string.includes("Odin")) return "Odin"
+//     else if (string.includes("Winterwunderland Candy Cane")) return "Winter-wunderland Candy Cane"
+//     //else return string
+// }
+
+function weaponNameFromString(string) {
+    const weapons = ["Classic", "Shorty", "Frenzy", "Ghost", "Sheriff", "Stinger", "Spectre", "Bucky", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Marshal", "Outlaw", "Operator", "Ares", "Odin"];
+
+    for (const weapon of weapons) {
+        if (string.includes(weapon)) {
+            return weapon;
+        } else if(string.includes("Winterwunderland Candy Cane")) return "Winter-wunderland Candy Cane"
+    }
 }
 
 function triggerCollected(uuid){
@@ -346,7 +440,7 @@ function uploadBackUpKey() {
     }
 }
 
-//just testing here
+//just testing here (dont know h´what exactly, combining backup key with new key?)
 // function merge(jsonString1, jsonString2) {
 //     // Parse the JSON strings into objects
 //     var obj1 = JSON.parse(jsonString1);
@@ -459,41 +553,9 @@ filterInput.addEventListener('input', function() {
   }
 });
 
-async function startPage(){
-    let jsonString_viewMode = localStorage.getItem('valodex_viewMode');
-    if(jsonString_viewMode == null) localStorage.setItem('valodex_viewMode', '{"mode": "list"}')
-    if(jsonString_viewMode == '{"mode": "grid"}'){
-        let image_viewMode = document.getElementById("viewmode_icon");
-        changeViewMode(image_viewMode);
-    }
 
 
-    let jsonString = localStorage.getItem('valodex_collected');
-    if(jsonString == null) localStorage.setItem('valodex_collected', '{"vdex_version": "1"}')
-
-    try {
-        await getApiThemes();
-        try {
-            await getApiSkins();
-            setTimeout(function() {
-                addThemesToWebsite();
-
-                const counter = document.getElementById("counter");
-                counter.textContent = updateCollectedCounter() + "/" + (skinArray.length - 2*18);
-                imageZoom();
-                sortThemes();
-              }, 1000);                  //your internet might me to slow and this website isn't optimized
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-startPage()
-
-
+// weapon counter (absolute and in percent)
 var counterElement = document.getElementById('counter');
 
 counterElement.addEventListener('mouseover', function() {
